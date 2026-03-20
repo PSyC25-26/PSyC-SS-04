6d2ac9c56e18af19aab3a7d0ba5a83276164acf7
REDoneBP98 <mikel.o.uria@opendeusto.es>
2026-03-20 10:40:08 +0100

    Creado el proyecto en Springboot

---
+++ game-comparer/src/main/java/com/ComparaJuegos/game_comparer/GameComparerApplication.java
@@ -0,0 +1,13 @@
+package com.ComparaJuegos.game_comparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameComparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameComparerApplication.class, args);
+	}
+
+}
---
+++ game-comparer/src/test/java/com/ComparaJuegos/game_comparer/GameComparerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ComparaJuegos.game_comparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameComparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
