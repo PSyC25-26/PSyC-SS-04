5681a5484cfad1c94900b2e9c3ee56672d76211e
REDoneBP98 <mikel.o.uria@opendeusto.es>
2026-03-23 22:36:13 +0100

    Añadida funcionalidad al registro de usuario mediante Thymeleaf, ahora los datos se guardan en una BD, creado con JPA. IMPORTANTE, debeis crear vosotros la BD en MysQl workbench u otro parecido, y luego springboot hace el resto

--- game-comparer/src/main/java/com/ComparaJuegos/game_comparer/controladores/controladorSesiones.java
+++ game-comparer/src/main/java/com/ComparaJuegos/game_comparer/controladores/controladorSesiones.java
@@ -1,12 +1,20 @@
 package com.ComparaJuegos.game_comparer.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ComparaJuegos.game_comparer.UsuarioRepositorio;
+import com.ComparaJuegos.game_comparer.models.Usuario;
+
+
 
 
 @Controller
 public class controladorSesiones {
-    
+
     @GetMapping("/iniciar")
 
     public String inicio(){
@@ -18,11 +26,28 @@ public class controladorSesiones {
         return "inicioSesion";
     }
 
+    //Dirección al formulario a secas
     @GetMapping("/registro")
-    public String registrarse() {
+    public String registrarse(Model model) {
+        model.addAttribute("usuario", new Usuario());
         return "registro";
     }
-    
 
+    //Necesario para que funcione lo siguiente, sino se queja
+    private final UsuarioRepositorio usuarioRepositorio;
+
+    controladorSesiones(UsuarioRepositorio usuarioRepositorio) {
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    //Esto recibe lo del formulario y lo guarda
+    @PostMapping("/registro")
+    public String registrarUsuario(@ModelAttribute Usuario usuario) {
+        //TODO: process POST request
+        usuarioRepositorio.save(usuario);
+        return "redirect:/inicioSesion";
+    }
+    
+    
  
 }
--- game-comparer/src/main/java/com/ComparaJuegos/game_comparer/models/Usuario.java
+++ game-comparer/src/main/java/com/ComparaJuegos/game_comparer/models/Usuario.java
@@ -1,75 +1,25 @@
 package com.ComparaJuegos.game_comparer.models;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 
 @Entity
-
+@Data
 @Table(name = "Usuario")
 public class Usuario {
 
     @Id //Esto debe estar siempre, pues la BD necesita un ID para funcionar
-    private long Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Ahora el ID se genera eutomaticamente
+    private long id;
 
-    private Date fecha_nac;
-    private String name, email, contraseña, pais;
+    private LocalDate fecha_nac;
+    private String name, email, contrasena, pais;
 
-    public Usuario() {
-        
-    }
-
-    public long getId() {
-        return Id;
-    }
-
-    public void setId(long id) {
-        Id = id;
-    }
-
-    public Date getFecha_nac() {
-        return fecha_nac;
-    }
-
-    public void setFecha_nac(Date fecha_nac) {
-        this.fecha_nac = fecha_nac;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-
-    
 }
