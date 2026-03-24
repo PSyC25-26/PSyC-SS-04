3f60fa7202ba8f4e9860c91aa20845b6bcae8443
TxemaSantamaria <josemaria.santamaria@opendeusto.es>
2026-03-24 14:28:15 +0100

    Añadidas nuevas dependencias de Spring Security para el log in

--- game-comparer/src/main/java/com/ComparaJuegos/game_comparer/UsuarioRepositorio.java
+++ game-comparer/src/main/java/com/ComparaJuegos/game_comparer/UsuarioRepositorio.java
@@ -3,7 +3,8 @@ package com.ComparaJuegos.game_comparer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ComparaJuegos.game_comparer.models.Usuario;
+import java.util.Optional;
 
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
-    
+    Optional<Usuario> findByEmail(String email); //Funcion para en el log in buscar por email
 }
