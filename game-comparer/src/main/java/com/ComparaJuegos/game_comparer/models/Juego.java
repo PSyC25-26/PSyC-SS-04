17379571045107761d580c71fc46ee83ad168497
REDoneBP98 <mikel.o.uria@opendeusto.es>
2026-04-14 20:20:04 +0200

    Implementados cambios en el css y modificado unos detalles al añadir juegos a la wishlist de un usuario

--- game-comparer/src/main/java/com/ComparaJuegos/game_comparer/models/Juego.java
+++ game-comparer/src/main/java/com/ComparaJuegos/game_comparer/models/Juego.java
@@ -1,14 +1,24 @@
 package com.ComparaJuegos.game_comparer.models;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
 @Entity
 @Data
-@Table(name = "Juego")
+@Table(name = "juego")
 public class Juego {
 
     @Id
@@ -19,11 +29,15 @@ public class Juego {
     private String publisher;
     private String developer;
     private String genero;
+
+    @Lob
+    @Column(columnDefinition="TEXT")
     private String descripcion;
+
     private String imagen;
     private LocalDate releaseDate;
 
-    @OneToMany(mappedBy = "juego", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "juego", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Precio> precios = new ArrayList<>();
 
     @ManyToMany(mappedBy = "juegos")
