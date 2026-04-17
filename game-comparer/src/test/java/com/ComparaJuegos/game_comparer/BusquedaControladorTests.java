71f226a22ec829dc89aeff100ed32313654cea51
REDoneBP98 <mikel.o.uria@opendeusto.es>
2026-04-17 20:27:51 +0200

    Implementados 2 tests más de busqueda controlador en BusquedaControladorTest

--- game-comparer/src/test/java/com/ComparaJuegos/game_comparer/BusquedaControladorTests.java
+++ game-comparer/src/test/java/com/ComparaJuegos/game_comparer/BusquedaControladorTests.java
@@ -1,16 +1,27 @@
 package com.ComparaJuegos.game_comparer;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
 
 import com.ComparaJuegos.game_comparer.controladores.BusquedaControlador;
 import com.ComparaJuegos.game_comparer.dto.ResultadoBusquedaDTO;
+import com.ComparaJuegos.game_comparer.models.Usuario;
 import com.ComparaJuegos.game_comparer.service.BusquedaService;
 
 
@@ -26,11 +37,17 @@ import com.ComparaJuegos.game_comparer.service.BusquedaService;
 		@Mock
 		private WishlistRepositorio wishlistRepositorio;
 
+		@Mock
+		private UserDetails detallesUsuario;
+
+		@Mock
+		private Model modelo;
+
 		@InjectMocks
 		private BusquedaControlador controlador;
 
-		@Test
-		void agreagarAWishlist(){
+		@Test//Revisar
+		void testAgreagarAWishlist(){
 
 			ResultadoBusquedaDTO dto_resul = new ResultadoBusquedaDTO();
 			long id_wishlist = 1L;
@@ -42,5 +59,51 @@ import com.ComparaJuegos.game_comparer.service.BusquedaService;
 			assertEquals("redirect:/wishlist/99", resultado);
 
 			verify(busquedaService).agregarAWishlist(dto_resul, id_wishlist);
+		}
+
+		@Test//Cuando acierta lo que se busca NO es null
+		void testBuscarJuegos(){
+			String id = "Battlefield";
+
+			when(detallesUsuario.getUsername()).thenReturn("userTest@gmail.com");
+
+			Usuario usuario = new Usuario();
+			when(usuarioRepositorio.findByEmail("userTest@gmail.com")).thenReturn(Optional.of(usuario));
+
+			List<ResultadoBusquedaDTO> resultadosTest = List.of(new ResultadoBusquedaDTO());
+			when(busquedaService.buscar(id)).thenReturn(resultadosTest);
+
+			when(wishlistRepositorio.findByUsuario(usuario)).thenReturn(List.of());
+
+			String vista = controlador.buscar(id, detallesUsuario, modelo);
+
+			verify(busquedaService).buscar(id);
+			verify(modelo).addAttribute("q", id);
+			verify(modelo).addAttribute("resultados", resultadosTest);
+
+			assertEquals("buscar", vista);
+
+		}
+
+		@Test //Ahora cuando lo que se busca ES null
+		void busquedaNull(){
+			when(detallesUsuario.getUsername()).thenReturn("usuarioTest@gmail.com");
+
+			String q = null;
+
+			Usuario usuario = new Usuario();
+
+			when(usuarioRepositorio.findByEmail("usuarioTest@gmail.com")).thenReturn(Optional.of(usuario));
+
+			when(wishlistRepositorio.findByUsuario(usuario)).thenReturn(List.of());
+
+			String vista = controlador.buscar(q, detallesUsuario, modelo);
+
+			verify(busquedaService, never()).buscar(any());
+			verify(modelo).addAttribute("q", q);
+			verify(modelo).addAttribute(eq("resultados"), anyList());
+			verify(modelo).addAttribute(eq("wishlists"), anyList());
+
+			assertEquals("buscar", vista);
 		}
 	}
