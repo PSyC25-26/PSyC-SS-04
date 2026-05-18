a55632271384f25330c31934f773b9e134fb3260
TxemaSantamaria <josemaria.santamaria@opendeusto.es>
2026-05-18 20:13:17 +0200

    Pagina inicio testeo en desarrollo

---
+++ game-comparer/src/test/java/UserInterface/principalUserInterfaceTest.java
@@ -0,0 +1,69 @@
+package UserInterface;
+
+import com.ComparaJuegos.game_comparer.GameComparerApplication;
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@SpringBootTest(
+        classes = com.ComparaJuegos.game_comparer.GameComparerApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "spring.main.allow-bean-definition-overriding=true"
+        }
+
+)
+@TestPropertySource(locations = "file:src/test/resources/application-test.properties.example")
+@ActiveProfiles("test")
+public class principalUserInterfaceTest {
+    @LocalServerPort
+    private int port;
+    private static Playwright playwright;
+    private static Browser navegador;
+    private BrowserContext contexto;
+    private Page pagina;
+
+    @BeforeAll
+    static void definirNavegador() {
+        playwright = Playwright.create();
+        navegador=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @BeforeEach
+    void crearContexto() {
+        contexto=navegador.newContext();
+        pagina=contexto.newPage();
+    }
+
+    @Test
+    @DisplayName("Testeando el botón del registro")
+    void testBotonRegistro() {
+        pagina.navigate("http://localhost:" + port + "/registro");
+        pagina.click("text=REGISTRATE");
+        assertTrue(pagina.url().contains("/registro"));
+    }
+
+    @Test
+    @DisplayName("Testeando botón de inicio de sesión")
+    void testBotonInicioSesion() {
+        pagina.navigate("http://localhost:" + port + "/inicioSesion");
+        pagina.click("text=INICIA SESIÓN");
+        assertTrue(pagina.url().contains("/inicioSesion"));
+    }
+    @AfterEach
+    void cerrarContexto() {
+        contexto.close();
+    }
+
+    @AfterAll
+    static void cerrarNavegador() {
+        navegador.close();
+        playwright.close();
+    }
+
+
+}
