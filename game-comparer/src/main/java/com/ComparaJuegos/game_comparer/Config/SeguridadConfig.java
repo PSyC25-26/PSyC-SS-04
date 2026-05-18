10d441fa79164993bb1ba2075b60fe35b726129e
TxemaSantamaria <josemaria.santamaria@opendeusto.es>
2026-05-18 16:00:22 +0200

    Iniciado el desarrollo de los test de UI

--- game-comparer/src/main/java/com/ComparaJuegos/game_comparer/Config/SeguridadConfig.java
+++ game-comparer/src/main/java/com/ComparaJuegos/game_comparer/Config/SeguridadConfig.java
@@ -2,7 +2,6 @@ package com.ComparaJuegos.game_comparer.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,20 +10,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile("!test")
 public class SeguridadConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/registro", "/inicioSesion", "/iniciar", "/css/**", "/estilos.css")
-                        .permitAll()// Sin sesion puede acceder a registro y login
-                        .anyRequest().authenticated()// Sin sesion al resto de paginas no puede acceder
+                        .requestMatchers("/", "/registro", "/inicioSesion", "/iniciar", "/css/**", "/estilos.css").permitAll()// Sin sesion puede acceder a registro y login
+                        .anyRequest().authenticated()//Sin sesion al resto de paginas no puede acceder
                 )
                 .formLogin(form -> form
-                        .loginPage("/inicioSesion") // Escucha en el iniciosesion mediante el form implementado ahi
-                        .defaultSuccessUrl("/perfil", true) // A donde vas al iniciar sesion
-                        .permitAll())
+                        .loginPage("/inicioSesion") //Escucha en el iniciosesion mediante el form implementado ahi
+                        .defaultSuccessUrl("/buscar", true) // A donde vas al iniciar sesion
+                        .permitAll()
+                )
                 .logout(logout -> logout.permitAll());
 
         return http.build();
